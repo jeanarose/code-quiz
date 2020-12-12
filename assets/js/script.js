e5f08a1ac04a7065c75d8d3fdf831021f28b6968
Jeana Rose <jr.mathis608@gmail.com>
2020-12-12 15:09:58 -0500

    Added questions array and question tracker to use in for loop in the function that writes the next question.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,18 +3,24 @@
 // 2. Add content
 // 3. Append to an existing element
 
-// First question object
-var firstQuestionObject = {
+// Question objects
+var firstQuestion = {
   answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
   question: "Commonly used data types do NOT include:",
   correctAnswer: "3. alerts"
 };
 
+var questionTracker = 0;
+
+var questionsArray = [
+  firstQuestion
+]
+
 // Variables that hold existing elements.
 var startButton = document.querySelector("button");
 var headingDisplay = document.querySelector("h1");
 var paragraphDisplay = document.querySelector("p");
-var ulElementAnswerChoices = document.getElementById("answer-choices");
+var ulElement = document.getElementById("answer-choices");
 var outcomeDisplay = document.querySelector("#outcome");
 
 
@@ -23,37 +29,40 @@ function startQuiz(event) {
   event.preventDefault();
 
   // Change heading to display question and remove paragraph text.
-  headingDisplay.textContent = firstQuestionObject.question;
+  headingDisplay.textContent = firstQuestion.question;
   headingDisplay.setAttribute("style", "text-align: left");
   paragraphDisplay.textContent = "";
 
   // Remove Start Quiz button from page.
   startButton.setAttribute("style", "display:none");
-  
+
+  // Writes the next question
   writeNextQuestion();
 }
-function writeNextQuestion(event){
-  event.preventDefault();
-  
+function writeNextQuestion(){
+   // Add horizontal line
+  //  if (event.target.matches("button")) {
+  //   outcomeDisplay.setAttribute("style", "display: block");
+  // }
+  for(var i = 0; i < questionsArray[questionTracker].answers.length; i++){
+    // Create li and append it to ul
+    var liElement = document.createElement("li");
+    liElement.textContent = "";
+    ulElement.append(liElement);
+
+    // Create button and append it to li
+    var answerButton = document.createElement("button");
+    answerButton.className = "btn";
+    answerButton.textContent = questionsArray[questionTracker].answers[i];
+    liElement.append(answerButton);
+  }
 }
 
 // Event listener for Start Button
 startButton.addEventListener("click", startQuiz);
 
-function firstQuestion(event) {
-  // Add horizontal line
-  if (event.target.matches("button")) {
-    outcomeDisplay.setAttribute("style", "display: block");
-  }
-  // Conditional to check if the answer was right or wrong
-  if(event.target.matches("button") === firstQuestionObject.correctAnswer){
-    // If what is inside button equals firstQuestionObject.correctAnswer
-    console.log("You clicked the correct answer!")
-  }
-}
 
 // Event listener for 'ul' element with 'li' answer choices.
-ulElementAnswerChoices.addEventListener("click", firstQuestion);
 
 // - DOM manipulation to change the h1 and paragraph text
 // - DOM manipulation to create answer choice buttons
