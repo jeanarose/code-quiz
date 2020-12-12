873294036aea99d94d10928ae1bd56d854857a3c
Jeana Rose <jr.mathis608@gmail.com>
2020-12-12 14:43:52 -0500

    Removed contents inside of startQuiz with the exception of the piece that clears the content of the first page. Removed creation of buttons and lis because it was not dynamic.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,6 +7,7 @@
 var firstQuestionObject = {
   answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
   question: "Commonly used data types do NOT include:",
+  correctAnswer: "3. alerts"
 };
 
 // Variables that hold existing elements.
@@ -16,27 +17,6 @@ var paragraphDisplay = document.querySelector("p");
 var ulElementAnswerChoices = document.getElementById("answer-choices");
 var outcomeDisplay = document.querySelector("#outcome");
 
-// Create 'li' elements and  buttons.
-var firstAnswerChoice = document.createElement("li");
-var firstAnswerButton = document.createElement("button");
-var secondAnswerChoice = document.createElement("li");
-var secondAnswerButton = document.createElement("button");
-var thirdAnswerChoice = document.createElement("li");
-var thirdAnswerButton = document.createElement("button");
-var fourthAnswerChoice = document.createElement("li");
-var fourthAnswerButton = document.createElement("button");
-
-// Text content for 'li's is set to empty
-firstAnswerChoice.textContent = "";
-secondAnswerChoice.textContent = "";
-thirdAnswerChoice.textContent = "";
-fourthAnswerChoice.textContent = "";
-
-// Add class to buttons
-firstAnswerButton.className = "btn";
-secondAnswerButton.className = "btn";
-thirdAnswerButton.className = "btn";
-fourthAnswerButton.className = "btn";
 
 // Function for 'Start Button'
 function startQuiz(event) {
@@ -47,40 +27,14 @@ function startQuiz(event) {
   headingDisplay.setAttribute("style", "text-align: left");
   paragraphDisplay.textContent = "";
 
-  // Append 'li' to 'ul' that holds the first answer choice button.
-  ulElementAnswerChoices.append(firstAnswerChoice);
-
-  // Create and append button to 'li' that displays the first answer choice.
-  // Add class for wrong answer.
-  firstAnswerButton.textContent = firstQuestionObject.answers[0];
-  firstAnswerChoice.append(firstAnswerButton);
-
-  // Create and append 'li' to 'ul' that holds the second answer choice button.
-  ulElementAnswerChoices.append(secondAnswerChoice);
-
-  // Create and append button to 'li' that displays the second answer choice.
-  // Add class for wrong answer.
-  secondAnswerButton.textContent = firstQuestionObject.answers[1];
-  secondAnswerChoice.append(secondAnswerButton);
-
-  // Create and append 'li' to 'ul' that holds the third answer choice button.
-  ulElementAnswerChoices.append(thirdAnswerChoice);
-
-  // Create and append button to 'li' that displays the third answer choice.
-  // Add class for correct answer.
-  thirdAnswerButton.textContent = firstQuestionObject.answers[2];
-  thirdAnswerChoice.append(thirdAnswerButton);
-
-  // Create and append 'li' to 'ul' that holds the fourth answer choice button.
-  ulElementAnswerChoices.append(fourthAnswerChoice);
-
-  // Create and append button to 'li' that displays the fourth answer choice.
-  // Add class for wrong answer.
-  fourthAnswerButton.textContent = firstQuestionObject.answers[3];
-  fourthAnswerChoice.append(fourthAnswerButton);
-
   // Remove Start Quiz button from page.
   startButton.setAttribute("style", "display:none");
+  
+  writeNextQuestion();
+}
+function writeNextQuestion(event){
+  event.preventDefault();
+  
 }
 
 // Event listener for Start Button
@@ -88,22 +42,14 @@ startButton.addEventListener("click", startQuiz);
 
 function firstQuestion(event) {
   // Add horizontal line
-  if (event.target.matches("li")) {
+  if (event.target.matches("button")) {
     outcomeDisplay.setAttribute("style", "display: block");
   }
-
   // Conditional to check if the answer was right or wrong
-  // if (correctAnswer) {
-  //   var correctDisplay = document.createElement("p");
-  //   correctDisplay.textContent = "Correct!";
-  //   outcomeDisplay.append(correctDisplay);
-  //   correctDisplay.setAttribute("style", "text-align: left");
-  //   // take user to the next page
-  // } else {
-
-  //   // take user to the next page
-  //   // deduct 10 seconds
-  // }
+  if(event.target.matches("button") === firstQuestionObject.correctAnswer){
+    // If what is inside button equals firstQuestionObject.correctAnswer
+    console.log("You clicked the correct answer!")
+  }
 }
 
 // Event listener for 'ul' element with 'li' answer choices.
