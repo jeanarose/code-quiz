1b9832c6c3efb1735ac6f191bc3b30a86c21194d
Jeana Rose <jr.mathis608@gmail.com>
2020-12-12 17:20:09 -0500

    Added four more question objects

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,14 +5,38 @@
 
 // Question objects
 var firstQuestion = {
-  answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
   question: "Commonly used data types do NOT include:",
-  correctAnswer: "3. alerts",
+  answers: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+  correctAnswer: "3. alerts"
 };
+
+var secondQuestion = {
+  question: "The condition in an if / else statement is enclosed within ____.",
+  answers: ["1. quotes", "2. curly brackets","3. parentheses", "4. square brackets"],
+  correctAnswer: "3. parentheses"
+}
+
+var thirdQuestion = {
+  question: "Arrays in JavaScript can be used to store ____.",
+  answers: ["1. numbers and strings","2. other arrays","3. booleans","4. all of the above"],
+  correctAnswer: "4. all of the above"
+}
+
+var fourthQuestion = {
+  question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+  answers: ["1. JavaScript","2. terminal / bash", "3. for loops", "4. console.log"],
+  correctAnswer: "4. console.log"
+}
+
+var fifthQuestion = {
+  question: "String values must be enclosed within ____ when being assigned to variables.",
+  answers: ["1. commas", "2. curly brackets","3. quotes","4. parentheses"],
+  correctAnswer: "3. quotes"
+}
 
 var questionTracker = 0;
 
-var questionsArray = [firstQuestion];
+var questionsArray = [firstQuestion, secondQuestion, thirdQuestion, fourthQuestion, fifthQuestion];
 
 // Variables that hold existing elements.
 var startButton = document.querySelector("button");
