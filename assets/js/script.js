f08180e218d5ba8902f4fde0f4a7d6eae35d2f99
Jeana Rose <jr.mathis608@gmail.com>
2020-12-12 10:11:52 -0500

    Added psuedo code to script.js

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+// WHEN I answer a question
+// THEN I am presented with another question
+
+// When the start button is clicked, the first question and answers appear
+// Will need:
+// - An event listener "click" for the Start Quiz button
+// - DOM manipulation to change the h1 and paragraph text
+// - DOM manipulation to create answer choice buttons 
+// - Event listeners on all of the answer choice buttons 
+// - Event listeners / DOM manipulation on each answer choice button such that
+//   when one is clicked, the next question and answers appear 
+
+
+
+// *** DO SECOND TO LAST ***
+// WHEN I click the start button
+// THEN a timer starts and I am presented with a question
+
+// WHEN I answer a question incorrectly
+// THEN time is subtracted from the clock
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+
+
+// *** DO LAST ***
+// WHEN the game is over
+// THEN I can save my initials and score
