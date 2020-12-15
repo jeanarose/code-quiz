623ef2935bb8f31267f1392c7ca7bb4508c820bf
Jeana Rose <jr.mathis608@gmail.com>
2020-12-15 16:26:14 -0500

    Made display variables global variables

--- assets/js/highscores.js
+++ assets/js/highscores.js
@@ -1,42 +1,53 @@
+// Global variables
+var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
+var initialsDisplay = document.getElementById("initials");
+var scoreDisplay = document.getElementById("score");
+var timeLeftDisplay = document.getElementById("time-left");
+
 // Function that writes the highscores
 function writeHighScores() {
-    var highScores = JSON.parse(localStorage.getItem("highscores")) || [];
-    var initialsDisplay = document.getElementById("initials");
-    var scoreDisplay = document.getElementById("score");
-    var timeLeftDisplay = document.getElementById("time-left");
-    for (var i = 0; i < highScores.length; i++) {
+  for (var i = 0; i < highScores.length; i++) {
     // Create, add text to, and append paragraph with initials
-      paragraphElementInitials = document.createElement("p");
-      paragraphElementInitials.textContent = highScores[i].initials;
-      paragraphElementInitials.className = "user-highscore"
-      initialsDisplay.append(paragraphElementInitials)
+    paragraphElementInitials = document.createElement("p");
+    paragraphElementInitials.textContent = highScores[i].initials;
+    paragraphElementInitials.className = "user-highscore";
+    initialsDisplay.append(paragraphElementInitials);
     // Create, add text to, and append paragraph with score
-      paragraphElementScore = document.createElement("p");
-      paragraphElementScore.textContent = highScores[i].score;
-      paragraphElementScore.className = "user-highscore"
-      scoreDisplay.append(paragraphElementScore);
+    paragraphElementScore = document.createElement("p");
+    paragraphElementScore.textContent = highScores[i].score;
+    paragraphElementScore.className = "user-highscore";
+    scoreDisplay.append(paragraphElementScore);
     // Create, add text to, and append paragraph with seconds left
-      paragraphElementTimeLeft = document.createElement("p");
-      paragraphElementTimeLeft.textContent = highScores[i].timeLeft;
-      paragraphElementTimeLeft.className = "user-highscore"
-      timeLeftDisplay.append(paragraphElementTimeLeft);
-    }
+    paragraphElementTimeLeft = document.createElement("p");
+    paragraphElementTimeLeft.textContent = highScores[i].timeLeft;
+    paragraphElementTimeLeft.className = "user-highscore";
+    timeLeftDisplay.append(paragraphElementTimeLeft);
   }
+}
 
-  writeHighScores();
+writeHighScores();
 
-  // Button variables
-  var goBackButton = document.getElementById("go-back");
-  var clearHighScoresButton = document.getElementById("clear-highscores");
+// Button variables
+var goBackButton = document.getElementById("go-back");
+var clearHighScoresButton = document.getElementById("clear-highscores");
+var highScoresList = document.getElementById("list-highscores");
 
-  // Function for Go Back button
-  function goBack(){
-    window.history.back();
-  }
+// Function for Go Back button
+function goBack() {
+  window.history.back();
+}
 
-  // Listener for Go Back button
-  goBackButton.addEventListener("click",goBack);
+// Listener for Go Back button
+goBackButton.addEventListener("click", goBack);
 
-  // Function for Clear Highscores Button
+// Function for Clear Highscores Button
+function clearHighScores() {
+  //   highScores = [];
+  localStorage.clear();
+  paragraphElementInitials.innerHTML = "";
+  paragraphElementScore.innerHTML = "";
+  paragraphElementTimeLeft.innerHTML = "";
+}
 
-  // Listener for Clear Highscores Button
+// Listener for Clear Highscores Button
+clearHighScoresButton.addEventListener("click", clearHighScores);
